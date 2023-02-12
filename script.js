03eb62ac9a1b6a6c4d3df416e3db1a4e8d10c3ed
OMARDEVS <85697878+omardevscom@users.noreply.github.com>
2023-02-12 17:19:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("square").
+    addEventListener("click", function() {
+        alert("You clicked the square");
+    }, false);
+
+
+document.getElementById("triangle").
+    addEventListener("click", function() {
+        alert("You clicked the triangle");
+    }, false);
+
+
+document.getElementById("circle").
+    addEventListener("click", function() {
+        alert("You clicked the circle");
+    }, false);
